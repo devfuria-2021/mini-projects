574c9da74e79f233b5c902c95d9a642208a26712
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-12-01 20:58:45 -0300

    project-02

--- project02/app.js
+++
@@ -1,9 +0,0 @@
-var element = document.getElementById('myimage');
-
-element.onclick = function() {
-    if (element.src.match("off")) {
-        element.src="lampada-on.gif";
-    } else {
-        element.src="lampada-off.gif";
-    }
-};
